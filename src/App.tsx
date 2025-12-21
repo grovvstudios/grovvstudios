35a9b424952b5c37a7c44b74662de441f34c7aab
grovvstudios <grovvstudios@gmail.com>
2025-12-21 13:39:08 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
 import Testimonials from "./components/testimonials";
 import { Process } from "./components/Process";
-import Services from "./components/services";
+import { Services } from "./components/services";
 import { VideoShowcase } from "./components/VideoShowcase"; 
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
