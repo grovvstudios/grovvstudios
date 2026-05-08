387f16358ab227d92dc43490ba75565c2f58d31f
grovvstudios <grovvstudios@gmail.com>
2026-05-08 23:38:01 +0530

    Update main.tsx with routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App.tsx";
+import RenaultCaseStudy from "./pages/RenaultCaseStudy.tsx";
+import "./index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/case-study/renault-duster" element={<RenaultCaseStudy />} />
+    </Routes>
+  </Router>
+);
